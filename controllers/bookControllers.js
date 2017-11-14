3192dcd16f20367629932c32a825d97ce5cfb4a2
aditReza <lete.note3@gmail.com>
2017-11-14 18:01:17 +0700

    CRUD Books

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -29,7 +29,42 @@ let findBook = function(req,res){
   })
 }
 
+let editBook = function(req,res){
+  let id = {
+    _id : ObjectId(req.params.id)
+  }
+  let updateBook = {
+    isbn : req.body.isbn,
+    title : req.body.title,
+    author : req.body.author,
+    category : req.body.category,
+    stock : req.body.stock
+  }
+  Books.findByIdAndUpdate(id, updateBook).then(function(){
+    res.status(200).send('1 document updated')
+    console.log('1 document updated')
+  }).catch(function(err){
+    res.status(500)
+    console.log(err)
+  })
+}
+
+let destroyBook = function(req,res){
+  let id = {
+    _id : ObjectId(req.params.id)
+  }
+  Books.findByIdAndRemove(id).then(function(){
+    res.status(200).send('1 document deleted')
+    console.log('1 document deleted')
+  }).catch(function(err){
+    res.status(500).send(err)
+    console.log(err)
+  })
+}
+
 module.exports = {
   createBook,
-  findBook
+  findBook,
+  editBook,
+  destroyBook
 }
--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,9 @@ const express = require('express')
 const books   = require('../controllers/bookControllers')
 const router  = express.Router()
 
-router.get('/', books.findBook)
 router.post('/', books.createBook)
+router.get('/', books.findBook)
+router.put('/:id', books.editBook)
+router.delete('/:id', books.destroyBook)
 
 module.exports = router
